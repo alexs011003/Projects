d59aa2ba850b8e4a8759d8728ec0ce95f62ae941
Alex Tso <alextso@AlexdeMacBook-Pro-2.local>
2026-01-24 21:33:54 -0500

    car control

--- Unit 1/Assets/Scripts/Drive.cs
+++ Unit 1/Assets/Scripts/Drive.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Drive : MonoBehaviour
 {
+    float forwardSpeed = 10.0f;
+    float rotateSpeed = 80.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,7 +12,27 @@ public class Drive : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        this.transform.Translate(Vector3.forward * Time.deltaTime);
+    {   
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //  this.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //}
+
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //  this.transform.Translate(Vector3.forward * Time.deltaTime * speed * -1);
+        //}
+
+        float forwardInput = Input.GetAxis("Vertical");
+        forwardInput = forwardInput * forwardSpeed;
+
+        this.transform.Translate(Vector3.forward * Time.deltaTime  * forwardInput);
+
+        float rotateInput = Input.GetAxis("Horizontal");
+        rotateInput *= rotateSpeed;
+        rotateInput *= Time.deltaTime;
+        this.transform.Rotate(0, rotateInput, 0);
+
     }
 }
